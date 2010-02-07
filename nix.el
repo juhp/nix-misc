bad5a2084cdfb2055c6ddc645b61f9e698f53320
Jens Petersen <juhpetersen gmail com>
2010-02-08 01:47:56 +1000

    here are nix-pkg and nixos-module

--- nix.el
+++ nix.el
@@ -9,3 +9,13 @@
   "Locate in nix store"
   (interactive (list (read-string "Nix locate: ")))
   (locate (concat "/nix/store/*" name "*")))
+
+(defun nix-pkg (name)
+  "Locate nixpkg *.nix "
+  (interactive (list (read-string "Nixpkg: ")))
+  (locate (concat "/etc/nixos/nixpkgs/pkgs/*" name "*.nix")))
+
+(defun nixos-module (name)
+  "Find nixos *.nix "
+  (interactive (list (read-string "Nixos module: ")))
+  (locate (concat "/etc/nixos/nixos/modules/*" name "*.nix")))
